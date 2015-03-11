73d47a0e4c45ae0be6a1f36bd6ffe136ec02614a
Ann Greyson <agreyson@fmbp-003803-ag.local>
2015-03-11 12:16:02 -0700

    Initial commit from local git

---
+++ TipCalculator2/ViewController.swift
@@ -0,0 +1,79 @@
+//
+//  ViewController.swift
+//  TipCalculator2
+//
+//  Created by Ann Greyson on 2/11/15.
+//  Copyright (c) 2015 Ann Greyson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var billAmountLabel: UILabel!
+    @IBOutlet weak var customTipPercentageLabel1: UILabel!
+    @IBOutlet weak var customTipPercentageLabel2: UILabel!
+    @IBOutlet weak var tipCustomLabel: UILabel!
+    @IBOutlet weak var totalCustomLabel: UILabel!
+    @IBOutlet weak var tip15Label: UILabel!
+    @IBOutlet weak var total15Label: UILabel!
+    @IBOutlet weak var customTipPercentageSlider: UISlider!
+    @IBOutlet weak var inputTextField: UITextField!
+    
+    let decimal100 = NSDecimalNumber(string: "100.0")
+    let decimal15Percent = NSDecimalNumber(string: "0.15")
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        // select inputTextField so keypad displays when the view loads
+        inputTextField.becomeFirstResponder()
+    }
+
+    @IBAction func calculateTip(sender: AnyObject) {
+        let inputString = inputTextField.text
+        let sliderValue = NSDecimalNumber(integer: Int(customTipPercentageSlider.value))
+        let customPercent = sliderValue/decimal100
+        if sender is UISlider {
+            customTipPercentageLabel1.text = NSNumberFormatter.localizedStringFromNumber(customPercent, numberStyle: NSNumberFormatterStyle.PercentStyle)
+            customTipPercentageLabel2.text = customTipPercentageLabel1.text
+        }
+        
+        if !inputString.isEmpty {
+            let billAmount = NSDecimalNumber(string: inputString)/decimal100
+            if sender is UITextField {
+                billAmountLabel.text = " " + formatAsCurrency(billAmount)
+                let fifteenTip = billAmount * decimal15Percent
+                tip15Label.text = formatAsCurrency(fifteenTip)
+                total15Label.text = formatAsCurrency(billAmount + fifteenTip)
+            }
+            let customTip = billAmount * customPercent
+            tipCustomLabel.text = formatAsCurrency(customTip)
+            totalCustomLabel.text = formatAsCurrency(billAmount + customTip)
+        }
+        else {
+            billAmountLabel.text = ""
+            tip15Label.text = ""
+            total15Label.text = ""
+            tipCustomLabel.text = ""
+            totalCustomLabel.text = ""
+        }
+    }
+}
+
+func formatAsCurrency(number: NSNumber) -> String {
+    return NSNumberFormatter.localizedStringFromNumber(number, numberStyle: NSNumberFormatterStyle.CurrencyStyle)
+}
+
+func +(left: NSDecimalNumber, right: NSDecimalNumber) -> NSDecimalNumber {
+    return left.decimalNumberByAdding(right)
+}
+func *(left: NSDecimalNumber, right: NSDecimalNumber) -> NSDecimalNumber {
+    return left.decimalNumberByMultiplyingBy(right)
+}
+func /(left: NSDecimalNumber, right: NSDecimalNumber) -> NSDecimalNumber {
+    return left.decimalNumberByDividingBy(right)
+}
+
+
